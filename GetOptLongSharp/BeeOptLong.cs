db81876319615c8564fdd65dfc3babb6244816d5
bee <bernhard.spindler.75@gmail.com>
2018-10-08 21:13:20 +0200

    first tests working

--- GetOptLongSharp/BeeOptLong.cs
+++ GetOptLongSharp/BeeOptLong.cs
@@ -1,54 +1,197 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace BeeOptLong
 {
+    public delegate void OnOption(string value);
+    public delegate void OnUnknownOption(string value);
+
+    public enum OPTTYPE
+    {
+        VALUE,
+        BOOL
+    }
+
     public class BeeOpts
     {
-        public readonly char   opt;
-        public readonly string optLong;
-        public readonly string desc;
-        public readonly Action<char,string> OnOption;
+        public readonly char        opt;
+        public readonly string      optLong;
+        public readonly OPTTYPE     type;
+        public readonly string      desc;
+        public readonly OnOption    OnOptionCallback;
 
-        public BeeOpts(char opt, string optLong, string desc, Action<char, string> OnOption)
+        private bool _wasFound;
+
+        public BeeOpts(char opt, string optLong, OPTTYPE type, string desc, OnOption OnOptionCallback)
         {
             this.opt = opt;
             this.optLong = optLong;
             this.desc = desc;
-            this.OnOption = OnOption;
+            this.type = type;
+            this.OnOptionCallback = OnOptionCallback;
+
+            this._wasFound = false;
+        }
+
+        public static bool Parse(string[] args, IList<BeeOpts> opts, out IList<string> parsedArgs, OnUnknownOption OnUnknown)
+        {
+            parsedArgs = new List<string>();
+            bool ok = true;
+
+            int i = 0;
+            while (i < args.Length)
+            {
+                string curr = args[i];
+
+                if ( curr.StartsWith("--") )
+                {
+                    if (curr.Length > 2)
+                    {
+                        ParseLong(args, opts, ref i, OnUnknown);
+                    }
+                    else
+                    {
+                        // ?? just grab the rest of the args and exit
+                    }
+                }
+                else if ( curr.StartsWith("-"))
+                {
+                    if (curr.Length > 1)
+                    {
+                        ParseShort(args, opts, ref i, OnUnknown);
+                    }
+                    else
+                    {
+                        throw new Exception($"bad option [{curr}]");
+                    }
+                }
+                else
+                {
+                    parsedArgs.Add(args[i]);
+                }
+                ++i;
+            }
+
+            return ok;
+        }
+        private static bool ParseShort(string[] args, IList<BeeOpts> opts, ref int i, OnUnknownOption OnUnknown)
+        {
+            bool ok = true;
+
+            string currArg = args[i];
+            int j = 1;
+
+            while (j < currArg.Length)
+            {
+                char curr = currArg[j];
+
+                BeeOpts foundOpt = opts.FirstOrDefault(o => o.opt == curr);
+                if ( foundOpt == null )
+                {
+                    OnUnknown(curr.ToString());
+                    ok = false;
+                }
+                else
+                {
+                    foundOpt._wasFound = true;
+
+                    if ( foundOpt.type == OPTTYPE.BOOL )
+                    {
+                        foundOpt.OnOptionCallback(null);
+                        ++j;
+                    }
+                    else if (foundOpt.type == OPTTYPE.VALUE)
+                    {
+                        if (j < currArg.Length - 1)     
+                        {
+                            foundOpt.OnOptionCallback(currArg.Substring(j + 1));    // rest is the value
+                        }
+                        else
+                        {
+                            string value = ReadNextAsArg(args, ref i);
+                            foundOpt.OnOptionCallback(value);
+                        }
+                        break;
+                    }
+                }
+            }
+            return ok;
+        }
+        private static void ParseLong(string[] args, IList<BeeOpts> opts, ref int i, OnUnknownOption OnUnknown)
+        {
+            string longOpt = args[i].Substring(2);
+
+            string[] optWithValue = longOpt.Split('=');
+
+            string optname;
+            if ( optWithValue.Length == 1 || optWithValue.Length == 2)
+            {
+                optname = optWithValue[0];
+            }
+            else
+            {
+                throw new Exception($"bad option [{longOpt}]");
+            }
+
+            BeeOpts foundOpt = opts.FirstOrDefault(o => o.optLong.Equals(optname));
+
+            if ( foundOpt == null )
+            {
+                OnUnknown?.Invoke(optname);
+            }
+            else
+            {
+                if (foundOpt.type == OPTTYPE.BOOL)
+                {
+                    foundOpt.OnOptionCallback(optname);
+                }
+                else if (foundOpt.type == OPTTYPE.VALUE)
+                {
+                    if (optWithValue.Length == 2)
+                    {
+                        foundOpt.OnOptionCallback(optWithValue[1]);
+                    }
+                    else
+                    {
+                        string value = ReadNextAsArg(args, ref i);
+                        foundOpt.OnOptionCallback(value);
+                    }
+                }
+            }
+        }
+        private static string ReadNextAsArg(string[] args, ref int i)
+        {
+            string value = (i+1) < args.Length ? args[i+1] : null;
+
+            if (value != null)
+            {
+                if (value.StartsWith("-") || value.StartsWith("--"))
+                {
+                    return null;
+                }
+            }
+
+            if ( value != null )
+            {
+                ++i;
+            }
+
+            return value;
         }
     }
     public class BeeOptsBuilder
     {
         private IList<BeeOpts> _data = new List<BeeOpts>();
 
-        public BeeOptsBuilder Add(char opt, string optLong, string desc, Action<char, string> OnOption)
+        public BeeOptsBuilder Add(char opt, string optLong, OPTTYPE type, string desc, OnOption OptionCallback)
         {
-            _data.Add(new BeeOpts(opt, optLong, desc, OnOption));
+            _data.Add(new BeeOpts(opt, optLong, type, desc, OptionCallback));
             return this;
         }
         public IList<BeeOpts> GetOpts()
         {
             return _data;
-        }
-    }
-
-    public class BeeOptsParser
-    {
-        public static IList<string> Parse(string[] args, IList<BeeOpts> opts)
-        {
-            IList<string> parsedArgs = new List<string>();
-
-            int i = 0;
-            while (i < args.Length )
-            {
-                if ( )
-
-                i++;
-            }
-
-
-            return parsedArgs;
         }
     }
 }
--- TestGetOptLong/TestBeeOptLong.cs
+++ TestGetOptLong/TestBeeOptLong.cs
@@ -12,10 +12,180 @@ namespace TestGetOptLong
         [TestMethod]
         public void NoOptions()
         {
-            IList<BeeOpts> opts = new List<BeeOpts>();
-            opts.Add()
+            var opts = new BeeOptsBuilder()
+                .GetOpts();
 
-            IList<string> args = BeeOpt.Parse(new string[] { "a", "b"}, )
+            bool ok = BeeOpts.Parse(
+                new string[] { "a", "b" }, 
+                opts, 
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+            Assert.AreEqual(2, args.Count);
+        }
+        [TestMethod]
+        public void OneOption()
+        {
+            string a = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('a', "add", OPTTYPE.VALUE, "add more args", (v) => a = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "-a", "b" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("b", a);
+        }
+        [TestMethod]
+        public void TwoValueOptions_one_not_given()
+        {
+            string r = null;
+            string o = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('o', "opt", OPTTYPE.VALUE, "desc", (v) => o = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "-r", "r", "-o" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("r", r);
+            Assert.IsNull(o);
+        }
+        [TestMethod]
+        public void TwoValueOptions_one_not_given_other_order()
+        {
+            string r = null;
+            string o = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "required", (v) => r = v)
+                .Add('o', "opt", OPTTYPE.VALUE, "optional", (v) => o = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "-o", "-r", "r" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("r", r);
+            Assert.IsNull(o);
+        }
+        [TestMethod]
+        public void TwoValueOptions_one_has_no_value()
+        {
+            string r = null;
+            string o = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('o', "opt", OPTTYPE.VALUE, "desc", (v) => o = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "-o", "-r", "r" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("r", r);
+            Assert.IsNull(o);
+        }
+        [TestMethod]
+        public void TwoOptions_bool_value()
+        {
+            string r = null;
+            bool b = false;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('b', "boo", OPTTYPE.BOOL,  "desc", (v) => b = true)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "-brR" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow {optname}"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("R", r);
+            Assert.IsTrue(b);
+        }
+        [TestMethod]
+        public void TwoOptions_bool_value_long_notation()
+        {
+            string r = null;
+            bool b = false;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('b', "boo", OPTTYPE.BOOL, "desc", (v) => b = true)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "--boo", "-rR" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow option [{optname}]"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("R", r);
+            Assert.IsTrue(b);
+        }
+        [TestMethod]
+        public void TwoOptions_long_short_with_value()
+        {
+            string r = null;
+            string b = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('b', "boo", OPTTYPE.VALUE, "desc", (v) => b = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "--boo=huu", "-r", "R" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow option [{optname}]"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("R", r);
+            Assert.AreEqual("huu", b);
+        }
+        [TestMethod]
+        public void TwoOptions_long_short_with_value_2()
+        {
+            string r = null;
+            string b = null;
+
+            var opts = new BeeOptsBuilder()
+                .Add('r', "req", OPTTYPE.VALUE, "desc", (v) => r = v)
+                .Add('b', "boo", OPTTYPE.VALUE, "desc", (v) => b = v)
+                .GetOpts();
+
+            bool ok = BeeOpts.Parse(
+                new string[] { "--boo", "huu", "-r", "R" },
+                opts,
+                out IList<string> args,
+                (optname) => throw new Exception($"unknow option [{optname}]"));
+
+            Assert.IsTrue(ok);
+            Assert.AreEqual("R", r);
+            Assert.AreEqual("huu", b);
         }
     }
 }
